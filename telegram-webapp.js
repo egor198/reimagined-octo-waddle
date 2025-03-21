d10b7a46c730ef694199aacbf8247075f850942d
John <105572869+egor198@users.noreply.github.com>
2025-03-21 19:40:12 +0700

    Add files via upload
    Test

---
+++ telegram-webapp.js
@@ -0,0 +1,21 @@
+// Инициализация Telegram Web App
+Telegram.WebApp.ready();
+
+// Функция для отправки данных в Unity
+function SendToUnity(data) {
+    // Если Unity загружен, отправляем сообщение
+    if (typeof unityInstance !== 'undefined') {
+        unityInstance.SendMessage('TelegramManager', 'OnMessageReceived', data);
+    }
+}
+
+// Функция для приёма данных из Unity
+function ReceiveFromUnity(data) {
+    // Отправляем данные в Telegram
+    Telegram.WebApp.sendData(data);
+}
+
+// Закрыть Web App по команде из Unity
+function CloseWebApp() {
+    Telegram.WebApp.close();
+}

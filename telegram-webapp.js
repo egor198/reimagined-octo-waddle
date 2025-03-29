7e06f2c53ae3aebb9a285a257ade60a001468276
John <105572869+egor198@users.noreply.github.com>
2025-03-29 20:25:08 +0700

    Add files via upload

--- telegram-webapp.js
+++ telegram-webapp.js
@@ -1,14 +1,21 @@
 Telegram.WebApp.ready();
 
-const user = Telegram.WebApp.initDataUnsafe.user;
-if (user) {
-    const userId = user.id;
-    const firstName = user.first_name || "Guest";
-    SendToUnity(JSON.stringify({ userId, firstName }));
-}
-
-function SendToUnity(data) {
-    if (typeof unityInstance !== 'undefined') {
-        unityInstance.SendMessage('TelegramManager', 'OnMessageReceived', data);
+function initTelegramData() {
+    const user = Telegram.WebApp.initDataUnsafe.user;
+    if (user) {
+        const userId = user.id.toString(); // Преобразуем в строку
+        const firstName = user.first_name || "Guest";
+        const userData = JSON.stringify({ userId, firstName });
+        
+        if (typeof SendToUnity === 'function') {
+            SendToUnity(userData);
+        } else if (typeof unityInstance !== 'undefined') {
+            unityInstance.SendMessage('TelegramManager', 'OnMessageReceived', userData);
+        } else {
+            console.error("Unity instance not ready");
+        }
     }
 }
+
+// Вызываем после небольшой задержки, чтобы Unity точно был готов
+setTimeout(initTelegramData, 500);
